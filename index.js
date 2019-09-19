593dd39a6d5b52a188ab11136cd98ebe1fc9de86
bubao <asd565586630@gmail.com>
2019-09-19 14:49:57 +0800

    更改入口

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const constant = require("./src/constant.js");
-const calendar = require("./src/calendar.js");
+/**
+ * @Description: 
+ * @Author: bubao
+ * @Date: 2019-09-11 11:06:17
+ * @LastEditors: bubao
+ * @LastEditTime: 2019-09-19 14:47:29
+ */
+const Calendar = require("./src/calendar.js");
 
-module.exports = {
-	calendar,
-	constant
-}
+module.exports = Calendar
